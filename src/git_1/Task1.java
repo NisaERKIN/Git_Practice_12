f2058cd17e2cc2306725f401cf213e14c437fd08
nisaerkin <nisaerkin@gmail.com>
2023-07-07 21:03:19 +0300

    Task1

---
+++ src/git_1/Task1.java
@@ -0,0 +1,8 @@
+package git_1;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hi Nisa");
+    }
+}
---
+++ src/git_1/Task3.java
@@ -0,0 +1,5 @@
+package git_1;
+
+public class Task3 {
+
+}
